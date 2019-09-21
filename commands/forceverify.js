2c64f2db1c581e90ca09df0b0fa69ec1227f2739
Austin Gallippo <phantomwolf154@gmail.com>
2019-09-21 19:44:41 -0400

    Update forceverify.js

--- commands/forceverify.js
+++ commands/forceverify.js
@@ -1,5 +1,30 @@
 module.exports.run = (bot, message, args, Discord) => {
  
+ 
+ 
+ 
+ if(message.member.roles.has("625085120809795634")) {
+  let member = message.mentions.members.first();
+ let memberid = member.id
+  let em = new Discord.RichEmbed()
+    .setTitle(`You have been force verified by: ${message.author.username} `)
+    .setColor("GREEN")
+    bot.users.get(memberid).send({embed: em})
+  //Verification message
+  
+  let role = message.guild.roles.get("625086210842558484")
+
+  
+  member.addRole(role);
+} else {
+  console.log(`You don't have permission to use this command.`);
+}
+ 
+ 
+ 
+ 
+ 
+ 
  let member = message.mentions.members.first();
  let memberid = member.id
   let em = new Discord.RichEmbed()
@@ -16,5 +41,5 @@ module.exports.run = (bot, message, args, Discord) => {
 }
 
 module.exports.help = {
-"name": "fverify"
+"name": "fver"
 }
