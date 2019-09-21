260a6a72a49c1963c43c82aba23354b1f855e853
Austin Gallippo <phantomwolf154@gmail.com>
2019-09-21 18:38:59 -0400

    Create verify.js

---
+++ commands/verify.js
@@ -0,0 +1,17 @@
+module.exports.run = (bot, message, args, Discord) => {
+  let em = new Discord.RichEmbed()
+  .setTitle("You have been verified!")
+  message.author.send({embed: em})
+  //Verification message
+  
+  let role = message.guild.roles.get("625086210842558484")
+
+  let member = message.member
+  
+  member.addRole(role);
+  //Add the role.
+}
+
+module.exports.help = {
+"name": "verify"
+}
