913d0d40557a67f2948eca48332c60f2111ab2ec
Austin Gallippo <phantomwolf154@gmail.com>
2019-09-21 19:39:04 -0400

    Create forceverify.js

---
+++ commands/forceverify.js
@@ -0,0 +1,20 @@
+module.exports.run = (bot, message, args, Discord) => {
+ 
+ let member = message.mentions.members.first();
+ let memberid = member.id
+  let em = new Discord.RichEmbed()
+    .setTitle("You have been force verified!")
+    .setColor("GREEN")
+    memberid.send({embed})
+  //Verification message
+  
+  let role = message.guild.roles.get("625086210842558484")
+
+  
+  member.addRole(role);
+  //Add the role.
+}
+
+module.exports.help = {
+"name": "fverify"
+}
